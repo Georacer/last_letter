e7bef1961690bfe63209b4d03936b27f6a9ff79d
Georacer <tailwhipx4@gmail.com>
2014-06-12 19:08:04 +0300

    re-touching quat2euler

--- last_letter/src/quat2euler.cpp
+++ last_letter/src/quat2euler.cpp
@@ -13,17 +13,21 @@ ros::Publisher pub;
 void translate_callback(last_letter::SimStates Odo)
 {
 	tf::Quaternion quat = tf::Quaternion(Odo.pose.orientation.x, Odo.pose.orientation.y, Odo.pose.orientation.z, Odo.pose.orientation.w);
-	if ( isnan(quat.length()) || (abs(quat.length())<0.9) ){ //check for invalid quaternion
+	if ( isnan(quat.length()) ) {
 		quat = tf::Quaternion(0, 0, 0, 1);
+		ROS_ERROR("quaternion length is NaN");
 	}
-	//quat.normalize();
-	//ROS_INFO("Quaterion: %g, %g, %g, %g, len: %g",quat[0], quat[1], quat[2], quat[3], quat.length()); //***
+	if (abs(quat.length())<0.9) {
+		ROS_ERROR("quaternion too short");
+		ROS_INFO("Quaterion: %g, %g, %g, %g, len: %g",quat[0], quat[1], quat[2], quat[3], quat.length());
+		quat.normalize();
+	}	
 	double phi,theta,psi;
 	tf::Matrix3x3(quat).getEulerYPR(psi, theta, phi);
 	geometry_msgs::Vector3 eulerVec;
-	eulerVec.x = phi*180/M_PI;
-	eulerVec.y = theta*180/M_PI;
-	eulerVec.z = psi*180/M_PI;
+	eulerVec.x = phi*180.0/M_PI;
+	eulerVec.y = theta*180.0/M_PI;
+	eulerVec.z = psi*180.0/M_PI;
 	pub.publish(eulerVec);
 }
 
@@ -31,7 +35,7 @@ int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "quat2euler");
 	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("states", 1000, translate_callback);
+	ros::Subscriber sub = n.subscribe("states", 1, translate_callback);
 	pub = n.advertise<geometry_msgs::Vector3>("euler", 1000);
 	ROS_INFO("quat2euler ready");
 	ros::spin();
