27e6cc0402ca03dfa527b10157a6851b3547f21a
Georacer <tailwhipx4@gmail.com>
2015-08-17 20:10:50 +0300

    Added mixing to the input channels.
    Re-compilation is needed for changes to apply.

--- last_letter/src/joy2chan.cpp
+++ last_letter/src/joy2chan.cpp
@@ -1,15 +1,11 @@
-#include <ros/ros.h>
-#include <sensor_msgs/Joy.h>
-#include <cstdlib>
-
-#include <last_letter_msgs/SimPWM.h>
+#include <joy2chan.hpp>
 
 ros::Publisher pub;
 
 int axisIndex[11];
 int buttonIndex[11];
 double throwIndex[11];
-
+int mixerid;
 
 void joy2chan(sensor_msgs::Joy joyMsg)
 {
@@ -28,19 +24,61 @@ void joy2chan(sensor_msgs::Joy joyMsg)
 		}
 	}
 
-	channels.value[0] = (unsigned int)(input[0]*500+ 1500); // Aileron channel
-	channels.value[1] = (unsigned int)(input[1]*500+ 1500); // Elevator channel
-	channels.value[2] = (unsigned int)((input[2]+1)*500+ 1000); // Throttle channel
-	channels.value[3] = (unsigned int)(input[3]*500+ 1500); // Rudder channel
-	channels.value[4] = (unsigned int)(input[4]*500 + 1500); // Steering gear channel
-	channels.value[5] = (unsigned int)(input[5]*1000 + 1000); // Breaks channel
-	channels.value[6] = (unsigned int)(input[6]*500 + 1500); // Generic channel
-	channels.value[7] = (unsigned int)(input[7]*500 + 1500); // Generic channel
-	channels.value[8] = (unsigned int)(input[8]*500 + 1500); // Generic channel
-	channels.value[9] = (unsigned int)(input[9]*1000 + 1000); // Init channel
-	channels.value[10] = (unsigned int)(input[10]*1000 + 1000); // Reset channel
+	channels = mixer(input, mixerid);
+	for (i=0;i<11;i++) // Cap channel limits
+	{
+		if (channels.value[i]<1000) channels.value[i]=1000;
+		if (channels.value[i]>2100) channels.value[i]=2100;
+	}
+
 	channels.header.stamp = ros::Time::now();
 	pub.publish(channels);
+}
+
+// Mixer function
+last_letter_msgs::SimPWM mixer(double * input, int mixerid)
+{
+	last_letter_msgs::SimPWM channels;
+	int i;
+	switch (mixerid)
+	{
+	case 0: // No mixing applied
+		for (i=0;i<11;i++)
+		{
+			channels.value[i] = (unsigned int)(input[i]*500 + 1500);
+		}
+		return channels;
+	case 1: // Airplane mixing
+		channels.value[0] = (unsigned int)(input[0]*500+ 1500); // Aileron channel
+		channels.value[1] = (unsigned int)(input[1]*500+ 1500); // Elevator channel
+		channels.value[2] = (unsigned int)((input[2]+1)*500+ 1000); // Throttle channel
+		channels.value[3] = (unsigned int)(input[3]*500+ 1500); // Rudder channel
+		channels.value[4] = (unsigned int)(input[4]*500 + 1500); // Steering gear channel
+		channels.value[5] = (unsigned int)(input[5]*1000 + 1000); // Breaks channel
+		channels.value[6] = (unsigned int)(input[6]*500 + 1500); // Generic channel
+		channels.value[7] = (unsigned int)(input[7]*500 + 1500); // Generic channel
+		channels.value[8] = (unsigned int)(input[8]*500 + 1500); // Generic channel
+		channels.value[9] = (unsigned int)(input[9]*1000 + 1000); // Init channel
+		channels.value[10] = (unsigned int)(input[10]*1000 + 1000); // Generic channel
+		return channels;
+	case 2: // Quadrotor mixing
+		channels.value[0] = (unsigned int)(500*((input[2]+1) -0.11*input[0] +0.11*input[1] -0.11*input[3]) +1000);
+		channels.value[1] = (unsigned int)(500*((input[2]+1) +0.11*input[0] +0.11*input[1] +0.11*input[3]) +1000);
+		channels.value[2] = (unsigned int)(500*((input[2]+1) -0.11*input[0] -0.11*input[1] +0.11*input[3]) +1000);
+		channels.value[3] = (unsigned int)(500*((input[2]+1) +0.11*input[0] -0.11*input[1] -0.11*input[3]) +1000);
+		channels.value[4] = (unsigned int)(input[6]*500 + 1500); // Generic channel
+		channels.value[5] = (unsigned int)(input[6]*500 + 1500); // Generic channel
+		channels.value[6] = (unsigned int)(input[6]*500 + 1500); // Generic channel
+		channels.value[7] = (unsigned int)(input[7]*500 + 1500); // Generic channel
+		channels.value[8] = (unsigned int)(input[8]*500 + 1500); // Generic channel
+		channels.value[9] = (unsigned int)(input[9]*1000 + 1000); // Init channel
+		channels.value[10] = (unsigned int)(input[10]*1000 + 1000); // Generic channel
+		return channels;
+	default:
+		ROS_FATAL("Invalid parameter for -/init/mixerid- in param server!");
+		ros::shutdown();
+
+	}
 }
 
 int main(int argc, char **argv)
@@ -70,6 +108,8 @@ int main(int argc, char **argv)
 		ROS_ASSERT(listInt[i].getType() == XmlRpc::XmlRpcValue::TypeInt);
 		buttonIndex[i]=listInt[i];
 	}
+	// Read the mixer type
+	if(!ros::param::getCached("init/mixerid", mixerid)) {ROS_INFO("No mixing function selected"); mixerid=0;}
 
 
 	// Enter spin
