565c65fe9c739600642a6ad7b9f50b15abdd0325
George Zogopoulos-Papaliakos <tailwhipx4@gmail.com>
2019-09-10 22:43:40 +0300

    Updated rqt_dashboard with last_letter_lib deps

--- rqt_dashboard/src/backend.cpp
+++ rqt_dashboard/src/backend.cpp
@@ -1,8 +1,12 @@
 #include <ros/ros.h>
-#include <nav_msgs/Odometry.h>
-#include <geometry_msgs/Vector3.h>
 #include <cstdlib>
 #include <math.h>
+#include <Eigen/Eigen>
+
+#include "uav_utils.hpp"
+#include "math_utils.hpp"
+
+#include <geometry_msgs/Vector3.h>
 #include <last_letter_msgs/SimStates.h>
 #include <last_letter_msgs/Environment.h>
 #include <rqt_dashboard/Dashboard.h>
@@ -16,9 +20,9 @@ last_letter_msgs::SimStates states;
 ros::Time tprev;
 double altPrev=0;
 
-void state_callback(last_letter_msgs::SimStates Odo)
+void state_callback(last_letter_msgs::SimStates p_state)
 {
-	states = Odo;
+	states = p_state;
 }
 
 void env_callback(last_letter_msgs::Environment env)
@@ -26,50 +30,28 @@ void env_callback(last_letter_msgs::Environment env)
 	environment = env;
 }
 
-void euler_callback(geometry_msgs::Vector3 euler)
-{
-	dash.euler = euler;
-}
-
-/////////////////////////////////////////
-//Aerodynamc angles/ airspeed calculation
-geometry_msgs::Vector3 getAirData (geometry_msgs::Vector3 speeds)
-{
-	double u = speeds.x-environment.wind.x;
-	double v = speeds.y-environment.wind.y;
-	double w = speeds.z-environment.wind.z;
-
-	double airspeed = sqrt(pow(u,2)+pow(v,2)+pow(w,2));
-	double alpha = atan2(w,u);
-	double beta = 0;
-	if (u==0) {
-		if (v==0) {
-			beta=0;
-		}
-		else {
-			beta=asin(v/abs(v));
-		}
-
-	}
-	else {
-		beta = atan2(v,u);
-	}
-
-	geometry_msgs::Vector3 result;
-	result.x = airspeed;
-	result.y = alpha*180/M_PI;
-	result.z = beta*180/M_PI;
-
-	return result;
-}
+// void euler_callback(geometry_msgs::Vector3 euler)
+// {
+// 	dash.euler = euler;
+// }
 
 void calculations(void)
 {
-	geometry_msgs::Vector3 tempVec;
-	tempVec = getAirData(states.velocity.linear);
-	dash.airspeed = tempVec.x;
-	dash.alpha = tempVec.y;
-	dash.beta = tempVec.z;
+	// Calculate relative air velocity vector
+	double x, y, z;
+	x = states.velocity.linear.x - environment.wind.x;
+	y = states.velocity.linear.y - environment.wind.y;
+	z = states.velocity.linear.z - environment.wind.z;
+	Eigen::Vector3d tempVec(x, y, z);
+	Eigen::Vector3d airdata = getAirData(tempVec);
+	dash.airspeed = airdata.x();
+	dash.alpha = airdata.y() * 180/M_PI;
+	dash.beta = airdata.z() * 180/M_PI;
+	Eigen::Quaterniond tempQuat(states.pose.orientation.w, states.pose.orientation.x, states.pose.orientation.y, states.pose.orientation.z);
+	Eigen::Vector3d eulVect(quat2euler(tempQuat));
+	dash.euler.x = eulVect.x() * 180/M_PI;
+	dash.euler.y = eulVect.y() * 180/M_PI;
+	dash.euler.z = eulVect.z() * 180/M_PI;
 	dash.climbRate = states.geoid.velocity.z;
 	dash.altitude = states.geoid.altitude;
 	dash.rotorspeed = states.rotorspeed[0]*60.0/2.0/M_PI; // Convert from RadPS to RPM
@@ -82,7 +64,7 @@ int main(int argc, char **argv)
 	ros::Rate spinner(100);
 	tprev = ros::Time::now();
 	ros::Subscriber subStates = n.subscribe("states", 1, state_callback);
-	ros::Subscriber subEuler = n.subscribe("euler", 1, euler_callback);
+	// ros::Subscriber subEuler = n.subscribe("euler", 1, euler_callback);
 	ros::Subscriber subEnv = n.subscribe("environment", 1, env_callback);
 	ros::Publisher pub = n.advertise<rqt_dashboard::Dashboard>("dashboard", 1);
 	// Initialize rotorspeed array
