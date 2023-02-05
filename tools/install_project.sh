9809feba7d1fc223e65f86fc3d2fea5a2e6327a5
George Zogopoulos-Papaliakos <tailwhipx4@gmail.com>
2023-02-05 16:21:46 +0100

    Rearranged models folders

--- tools/install_project.sh
+++ tools/install_project.sh
@@ -5,8 +5,8 @@ set -e
 # Set current working directory to the directory of the script
 cd "$(dirname "$0")"
 
-cd ../last_letter/external/last_letter_lib/models
+cd ~/last_letter_lib/models
 
 # Generate the model folders in the user home
-./skywalker_2013.py
+./skywalker_2013/skywalker_2013.py
 
